286bcc250a9e52490828e9e40b28e8b461ae9bd9
Adel Lassag <adetech.aadl@gmail.com>
2018-03-02 21:07:45 +0100

    set finished tasks

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var input = document.getElementById("input");
 var btn = document.getElementById("btn");
 var toDoList = document.getElementById("todo-list");
 var list = [];
+var index = document.getElementsByClassName("index");
+var retrievedTasks = localStorage.getItem("tasks");
 
 
 btn.addEventListener("click", function(){
@@ -10,12 +12,12 @@ btn.addEventListener("click", function(){
 			
 		list.push(input.value);
 
-		toDoList.innerHTML  += "<li>"+ list[list.length-1] +"</li>";
+		toDoList.innerHTML  += "<li class='index' id='"+(list.length-1)+"' onclick='done(this.id)'>"+ list[list.length-1] +"</li>";
 
-		input.value = "";
-			
+        input.value = "";
+		localStorage["tasks"] = JSON.stringify(list);
 	}
-           
+    
 });
 
 input.addEventListener("keydown", function (){
@@ -25,3 +27,22 @@ input.addEventListener("keydown", function (){
     }
     
 });
+
+
+function done(id){
+			
+	index[id].classList.toggle("checked");
+
+};
+
+if("tasks" in localStorage){
+
+	var retrievedTasks = localStorage.getItem("tasks");
+
+    list = JSON.parse(retrievedTasks);
+    for(var i=0; i<list.length ; i++){
+		toDoList.innerHTML  += '<li class="index"  onclick="done(this.id)" id="'+ i +'">' + list[i] +"</li>";
+	}
+		
+}
+
